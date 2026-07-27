de8648e58ed94347203a13b58b67dd559bc1145e
Alex <alex@laialex.com>
2026-07-27 14:27:50 +0800

    fix: update Python test to use zymeup_sdk package name

--- python/tests/test_version.py
+++ python/tests/test_version.py
@@ -1,5 +1,5 @@
-from shipzy_sdk import __version__
+from zymeup_sdk import __version__
 
 
-def test_version_is_alpha():
-    assert __version__ == "0.1.0-alpha.1"
+def test_version():
+    assert __version__ == "1.2.0"
